8dcb0aa9a41bf7b704a648039abd47000b7ed62a
Sagar ghare <sagar.ghare@acc.ltd>
2024-03-11 16:30:13 +0530

    protected route added using outlet and navigate

--- src/App.js
+++ src/App.js
@@ -1,42 +1,66 @@
 import 'animate.css';
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "./Component/Home";
 import Images from "./Component/Images";
 import Layout from "./Component/Layout";
 import NotFound from "./Component/404";
 import Login from "./Component/Login";
 import Profile from "./Component/Profile";
+import Protected from './Protected';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout><Home /></Layout>
-  },
-  {
-    path: '/images',
-    element: <Layout><Images /></Layout>
-  },
-  {
-    path: '/login',
-    element: <Layout><Login /></Layout>
-  },
-  {
-    path: '/profile',
-    element: <Layout><Profile /></Layout>
-  },
-  {
-    path: '/*',
-    element: <Layout><NotFound /></Layout>
-  }
-]);
+// OLD WAY TO HANDLE ROUTING
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout><Home /></Layout>
+//   },
+//   {
+//     path: '/images',
+//     element: <Layout><Images /></Layout>
+//   },
+//   {
+//     path: '/login',
+//     element: <Layout><Login /></Layout>
+//   },
+//   {
+//     path: '/profile',
+//     element: <Layout><Profile /></Layout>
+//   },
+//   {
+//     path: '/*',
+//     element: <Layout><NotFound /></Layout>
+//   }
+// ]);
 
+// const App = () => {
+//   return (
+//     <>
+//       <>
+//         <RouterProvider
+//           router={router}
+//         />
+//       </>
+//     </>
+//   );
+// }
+
+// NEW WAY TO HANDLE ROUTING
 const App = () => {
   return (
     <>
       <>
-        <RouterProvider
-          router={router}
-        />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Layout><Home /></Layout>} />
+            <Route path='/images' element={<Layout><Images /></Layout>} />
+            <Route element={<Protected />}>
+              <Route path='/profile' element={<Layout><Profile /></Layout>} />
+            </Route>
+            <Route path='/login' element={<Layout><Login /></Layout>} />
+            <Route path='/*' element={<Layout><NotFound /></Layout>} />
+          </Routes>
+        </BrowserRouter>
       </>
     </>
   );
